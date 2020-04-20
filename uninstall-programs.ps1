ea0170106000165073520f22fcfcc49e7526bd5c
jestrada <jestrada@lco.global>
2020-04-20 15:44:11 -0700

    first test pass of script

--- uninstall-programs.ps1
+++ uninstall-programs.ps1
@@ -1,5 +1,6 @@
-# Store Credential
-$credential = Get-Credential
+# download file from url with wget
+Invoke-WebRequest "https://drive.google.com/open?id=1zEUOVcY1BI8MQIpw0J2hTum2S8D_ufjv" -OutFile "C:\Users\Jesus"
 
-# Download from remote server
-Get-SCPFile -ComputerName "fsfs.lco.gtn" -Credential $credential -LocalFolder "C:\" -RemoteFolder "/library/software/VPN-Software/GVCSetup64_4.9.9.1016_EN.exe"
+
+# Start install process
+# Start-Process 'path/to/file' "/S"
